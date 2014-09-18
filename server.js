dd88f251028e31180521e8898f8b1424d9039e19
sql <reich.justin@gmail.com>
2014-09-18 15:11:43 -0700

    mongoose

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     stylus  = require('stylus'),
     logger  = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV =  process.env.NODE_ENV || "development";
 
@@ -23,6 +24,13 @@ app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }));
 
 //these are public so let it in
 app.use(express.static(__dirname + '/public'));
+
+mongoose.connect('mongodb://localhost/mean');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error'));
+db.once('open',function callback() {
+   console.log('mongo open');
+});
 
 app.get('/partials/:partialPath',function(req,res) {
    res.render('partials/' + req.params.partialPath)
