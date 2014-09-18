daac6ccfd4eedbf7fabc17bd2c329b9e040f8b5d
sql <reich.justin@gmail.com>
2014-09-18 14:38:43 -0700

    stylus

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
-var express = require('express');
+var express = require('express'),
+    stylus  = require('stylus'),
+    logger  = require('morgan'),
+    bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV =  process.env.NODE_ENV || "development";
 
 var app = express();
 
+/*
+ Stylus middleware
+ */
+function compile(str,path) {
+    return stylus(str).set('filename',path);
+};
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+
+app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }));
+
+//these are public so let it in
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req,res) {
     res.render('index');
