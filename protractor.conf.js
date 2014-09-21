4ce8825502d14bc39dbd08eb203ce056f5fcec2f
sql <reich.justin@gmail.com>
2014-09-20 19:23:52 -0700

    e2e for main

--- protractor.conf.js
+++ protractor.conf.js
@@ -5,25 +5,14 @@ exports.config = {
     ],
 
     chromeOnly: true,
-
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'args': ['show-fps-counter=true']
+        }
+    },
     baseUrl: '',
 
-    capabilities: {
-        'browserName': 'phantomjs',
-
-        /*
-         * Can be used to specify the phantomjs binary path.
-         * This can generally be ommitted if you installed phantomjs globally.
-         */
-        'phantomjs.binary.path': 'node_modules/phantomjs/bin/phantomjs',
-
-        /*
-         * Command line arugments to pass to phantomjs.
-         * Can be ommitted if no arguments need to be passed.
-         * Acceptable cli arugments: https://github.com/ariya/phantomjs/wiki/API-Reference#wiki-command-line-options
-         */
-        'phantomjs.cli.args': ['--logfile=PATH', '--loglevel=DEBUG']
-    },
 
     framework: 'mocha',
     mochaOpts: {
--- public/tests/e2e/main/main-e2e.spec.js
+++ public/tests/e2e/main/main-e2e.spec.js
@@ -1,9 +1,16 @@
-
+var chai=require('chai');
+var chaiAsPromised=require('chai-as-promised');
+chai.use(chaiAsPromised);
+var expect=chai.expect;
 
 describe('main layout page', function() {
-    it('should add one and two', function() {
+    it('should have a container with id main-content', function() {
+        //navigate to the index page
         browser.get('http://localhost:3030');
 
+        var mainContent = element.all(by.name('main-content'));
 
+        //there should be 1 main content container
+        expect(mainContent.count()).to.eventually.equal(1);
     });
 });
